67816bb0182999335387c167e0ab40d93cad6b38
FelipeCamargos <felipecamargos@gmail.com>
2013-12-04 12:33:37 -0200

    Adicionado pacote controleUsuariosTeste.

---
+++ src/UsuariosTeste/src/controleUsuariosTeste/ControleUsuariosTeste.java
@@ -0,0 +1,14 @@
+package controleUsuariosTeste;
+
+import telas.TelaCadastrarUsuario;
+
+public class ControleUsuariosTeste {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        TelaCadastrarUsuario telaCadastrarUsuario = new TelaCadastrarUsuario();
+    }
+}
